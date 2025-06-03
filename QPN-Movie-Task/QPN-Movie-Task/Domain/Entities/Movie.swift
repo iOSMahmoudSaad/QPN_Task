83a5f9037935e3da657b0eb66b97c93731eb803d
Mahmoud Saad <msaaad202020@gmail.com>
2025-06-03 18:33:37 +0300

    add Entitie ---> Domain

---
+++ QPN-Movie-Task/QPN-Movie-Task/Domain/Entities/Movie.swift
@@ -0,0 +1,35 @@
+//
+//  Movie.swift
+//  QPN-Movie-Task
+//
+//  Created by Mahmoud Saad on 03/06/2025.
+//
+
+import Foundation
+
+struct Movie: Identifiable, Codable {
+    let id: Int
+    let title: String
+    let overview: String
+    let posterPath: String?
+    let releaseDate: String?
+    let voteAverage: Double
+    var originalLanguage: String?
+    let voteCount : Int
+    var posterURL: URL? {
+        guard let posterPath = posterPath else { return nil }
+        return URL(string: "https://image.tmdb.org/t/p/w500\(posterPath)")
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case overview
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+        case originalLanguage = "original_language"
+
+    }
+}
